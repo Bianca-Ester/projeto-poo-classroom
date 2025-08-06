8f891865302e535afd7d9ae8ae527d8b28d99b93
Bianca-Ester <biancaester870@gmail.com>
2025-08-06 08:33:09 -0300

    Update pessoa.py

--- models/pessoa.py
+++ models/pessoa.py
@@ -1,14 +1,14 @@
 class Pessoa:
     def __init__(self, matricula, senha, id=None):
         self.matricula = matricula
-        self.__senha = senha
+        self.senha = senha
         self.id = id
 
     def to_dict(self):
         return{
             'id': self.id,
             'matricula': self.matricula,
-            'senha': self.__senha
+            'senha': self.senha
         }
 
 
