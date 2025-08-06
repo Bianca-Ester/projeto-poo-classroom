0ca46e952c452379b52fcc0f5f7519990d6c1e1f
Bianca-Ester <biancaester870@gmail.com>
2025-08-06 08:32:44 -0300

    Update turma.py

--- models/turma.py
+++ models/turma.py
@@ -1,15 +1,15 @@
 class Turma:
     def __init__(self, nome, senha, admin, id=None):
         self.nome = nome
-        self.__senha = senha
+        self.senha = senha
         self.admin = admin
         self.id = id
 
     def to_dict(self):
         return{
-            'id': self.id,
             'nome': self.nome,
-            'senha': self.__senha,
-            'administrador': self.admin
+            'senha': self.senha,
+            'administrador': self.admin,
+            'id': self.id
         }
 
